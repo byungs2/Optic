79f79bb11fb4994f26f9fcbc49bc390910bb96c2
byunghunlee <byungs286@gmail.com>
2024-05-29 18:42:46 +0900

    add: v4l2

--- main.cc
+++ main.cc
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <opencv2/core.hpp>
+#include <fcntl.h>
 
 int
 main (int argc, char *argv[])
 {
-  cv::Mat a;
+  int fd = 0;
   (void) sizeof (argc);
   (void) sizeof (*argv);
-  (void) sizeof (a);
-  std::cout << a << std::endl;
-  std::cout << "HELLO WORLD" << std::endl;
+
+  fd = open ("/dev/video0", O_RDWR);
+  std::cout << fd << std::endl;
+  std::cout << EACCES << std::endl;
+  std::cout << EBUSY << std::endl;
+  std::cout << ENXIO << std::endl;
+  std::cout << ENOMEM << std::endl;
+  std::cout << EMFILE << std::endl;
+  std::cout << ENFILE << std::endl;
   return 0;
 }
--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # you should make .pc file for dependency libraries to make build machine's pkg config properly find libs  
+export PKG_CONFIG_PATH=/home/byunghun/workspace/luckfox-pico/sysdrv/source/buildroot/buildroot-2023.02.6/output/host/arm-buildroot-linux-uclibcgnueabihf/sysroot/usr/lib/pkgconfig/
 
 rm -r builddir
 meson setup --cross-file arm.ini builddir
