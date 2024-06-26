35305a56a9ca216a3267f06f98f5de769ecfd16b
byunghunlee <byungs286@gmail.com>
2024-06-26 12:27:13 +0900

    add: ioctl

--- main.cc
+++ main.cc
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <opencv2/core.hpp>
 #include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/videodev2.h>
 
 int
 main (int argc, char *argv[])
 {
   int fd = 0;
+  int ret = 0;
+  struct v4l2_capability cap;
   (void) sizeof (argc);
   (void) sizeof (*argv);
 
   fd = open ("/dev/video0", O_RDWR);
+  ret = ioctl (fd, VIDIOC_QUERYCAP, &cap);
+
   std::cout << fd << std::endl;
   std::cout << EACCES << std::endl;
   std::cout << EBUSY << std::endl;
@@ -17,5 +23,16 @@ main (int argc, char *argv[])
   std::cout << ENOMEM << std::endl;
   std::cout << EMFILE << std::endl;
   std::cout << ENFILE << std::endl;
+  std::cout << ret << std::endl;
+
+  std::cout << cap.driver << std::endl;
+  std::cout << cap.card << std::endl;
+  std::cout << cap.bus_info << std::endl;
+  std::cout << ((cap.version >> 16) & 0xFF) << std::endl;
+  std::cout << ((cap.version >> 8) & 0xFF) << std::endl;
+  std::cout << (cap.version & 0xFF) << std::endl;
+  std::cout << cap.capabilities << std::endl;
+  std::cout << cap.device_caps << std::endl;
+  std::cout << cap.reserved << std::endl;
   return 0;
 }
